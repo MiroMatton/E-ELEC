e33e5d1dc70ca213ddf76b0f108bcaf0231b57ad
miromatton <miro.matton@student.odisee.be>
2022-04-23 11:07:39 +0200

    :art: header styles added

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,52 @@
 <script lang="ts">
-	let name: string = 'world';
+  let name: string = "world";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<header>
+  <div class="topBar">
+    <a href="tel:+32 69 420 666">+32 69 420 666</a>
+    <a
+      href="mailto: INFO@E-ELEC.BE
+    "
+      >INFO@E-ELEC.BE
+    </a>
+  </div>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+</header>
+<main />
 
 <style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+  header {
+    height: 70vh;
+    background: linear-gradient(-5deg, white 25%, #6bc9ef 25%);
+  }
 
-		h1 {
-			color: #ff3e00;
-			text-transform: uppercase;
-			font-size: 4em;
-			font-weight: 100;
-		}
+  .topBar {
+    background-color: #ef916b;
+    color: white;
+    display: flex;
+    align-items: flex-end;
+  }
 
-		@media (min-width: 640px) {
-			max-width: none;
-		}
-	}
+  .wrapper {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+      max-width: none;
+    }
+  }
 </style>
